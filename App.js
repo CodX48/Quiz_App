a4ab98e7d741e86921e57eb3c57e0aa6cc9505b4
codx207 <codx207@gmail.com>
2024-12-05 16:03:08 +0300

    i add user object that containes his informations like name and ANS, and i have added events to handel the user answer, my next step is to add calculater function to cal user result and i will add page represent the user name and his quiz result

--- App.js
+++ App.js
@@ -12,23 +12,50 @@ let Q ={
     difficulty:"easy",
     type:"multiple"
 }
+//information about the user
+
+let userInfo = {
+    name:"",
+    user_ans: []
+    ,score: 0
+}
 
 async function getQ() {
     const response = await fetch(`https://opentdb.com/api.php?amount=${Q.amount}&category=${Q.category}&difficulty=${Q.difficulty}&type=${Q.type}`);
     const data = await response.json();
     return data;
 }
-
+let exam_ans = []
 let sec = 0;
 let data;
-const buttons = document.querySelectorAll('button');
+function handelANS(){ 
+    let  x  = 0;
+    data.results.forEach(ele=>{
+        exam_ans.push({
+            id: x,
+            correct_answer: `${ele.correct_answer}`,
+        })
+        x++;
+    })
+}
 
+function handelUserANS(Ans){
+    userInfo.user_ans.push({
+        selectedANS:Ans
+    })
+
+}
+
+const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
   button.addEventListener('click', event => {
     if(button.getAttribute('key') == 'start'){
         setTimeout(async ()=>{
             data = await getQ();
+            handelANS();
+            //console.log(exam_ans)
             loadQ(data)
+            //console.log(data.results);
         } ,1000);
 
         
@@ -109,20 +136,21 @@ let Q_incrementer = 0;
 function loadQ(Data) {
     if (Data.response_code == '0' && Array.isArray(Data.results)) {
         questionsHandel(Q_incrementer, Data);
+        //console.log(Data)
     } else {
         console.log("No data or invalid format");
     }
 }
 
 function renderQuizSection(Question) {
-    let answersHTML = `<p>${Question.correct_answer}</p>`;
+    let answersHTML = `<p id="Quiz_Ans">${Question.correct_answer}</p>`;
     Question.incorrect_answers.forEach((ele) => {
-        answersHTML += `<p>${ele}</p>`;
+        answersHTML += `<p id="Quiz_Ans">${ele}</p>`;
     });
 
     return `
     <div class="Q-section">
-        <h2 class="main-Q">${Question.question}</h2>
+        <h2 class="main-Q" id="${Q_incrementer}">${Question.question}</h2>
         <div class="Answers-sec">${answersHTML}</div>
         <button class="quiz-btn" ${Q_incrementer === Q.amount - 1 ? 'key="lastQ"' : 'key="nextQ"'}>Next</button>
     </div>`;
@@ -130,6 +158,7 @@ function renderQuizSection(Question) {
 
 function questionsHandel(index, Data) {
     document.querySelector(".selection-list").innerHTML = renderQuizSection(Data.results[index]);
+    
 }
 
 document.querySelector(".selection-list").addEventListener('click', (event) => {
@@ -137,9 +166,32 @@ document.querySelector(".selection-list").addEventListener('click', (event) => {
         if (Q_incrementer < data.results.length - 1) {
             Q_incrementer++;
             loadQ(data);
+
         }
 
     } else if (event.target.getAttribute('key') === 'lastQ') {
         console.log("End of quiz!");
     }
 });
+
+document.getElementById('sendName').addEventListener("click",(Event)=>{
+ userInfo.name = document.getElementById('namehandel').value;
+ //console.log(userInfo.name);
+ Event.preventDefault();
+});
+
+// this function is made to calc handel the user ans
+document.addEventListener("click",(Event)=>{
+    if(Event.target.id == "Quiz_Ans"){
+        handelUserANS(Event.target.textContent)
+        //console.log()
+    }
+});
+
+document.addEventListener("click",(Event)=>{
+    if(Event.target.getAttribute('key') == 'lastQ'){
+        console.log(userInfo)
+        console.log(exam_ans)
+    }
+})
+
